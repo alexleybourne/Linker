51f7c9915a6127564b8d02928043d58753cf1759
alexleybourne <alexleybourne1@gmail.com>
2021-01-01 23:22:32 +1000

    Player now starts and pauses music

--- src/App.js
+++ src/App.js
@@ -8,11 +8,19 @@ function App() {
 
   const [songs, setSongs] = useState(data())
   const [currentSong, setCurrentSong] = useState(songs[0])
+  const [isPlaying, setIsPlaying] = useState(false)
 
   return (
     <div className="App">
-      <Song currentSong={currentSong} />
-      <Player />
+      <Song
+        isPlaying={isPlaying}
+        currentSong={currentSong}
+      />
+      <Player
+        isPlaying={isPlaying}
+        setIsPlaying={setIsPlaying}
+        currentSong={currentSong}
+      />
     </div>
   );
 }
