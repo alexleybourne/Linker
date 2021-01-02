dae4c93a5103c1e78e3b8b21e727471524d645e1
alexleybourne <alexleybourne1@gmail.com>
2021-01-03 06:54:51 +1000

    Can now select and play songs from library component

--- src/components/LibrarySong.js
+++ src/components/LibrarySong.js
@@ -1,9 +1,13 @@
 import { useRef, useState } from 'react'
 
-const LibrarySong = ({song}) => {
+const LibrarySong = ({song, songs, setCurrentSong}) => {
+
+    const songSelectHandler = () => {
+        setCurrentSong(song)
+    }
 
     return (
-        <div className="library-song">
+        <div onClick={songSelectHandler} className="library-song">
             <img src={song.cover} alt={song.name + ' cover art'}/>
             <div className="song-description">
                  <h3>{song.name}</h3>
